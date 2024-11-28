4327728fc2869107db9fd3f9f4659824c4abe460
vit303 <vitalyzaozernov@gmail.com>
2024-11-28 23:23:44 +1400

    Добавьте файлы проекта.

---
+++ practice4/IntegerNumbersArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice4
+{
+    internal class IntegerNumbersArray
+    {
+        private List<int> array = new List<int>();
+        private int count = 0;
+
+        public IntegerNumbersArray() {
+
+        }
+
+        public IntegerNumbersArray(int number)
+        {
+            array.Add(number);
+            this.count++;
+        }
+
+        public void AddNumber(int num) {
+            array.Add(num);
+            this.count++;
+        }
+
+        public List<int> GetList() {
+            return array;
+        }
+    }
+}
---
+++ practice4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<IntegerNumbersArray> array = new List<IntegerNumbersArray>();
+            for (int i = 0; i < 3; i++) {
+                
+                while (true) {
+                    try
+                    {
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        array[i].AddNumber(num);
+                    }
+                    catch {
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
